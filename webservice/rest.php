9a94236cecdad3aac75d2e8b859861a3a3310a00
valkrhlan <valkrhlan@foi.hr>
2016-11-06 23:59:58 +0100

    Dodana registracija volontera

--- webservice/rest.php
+++ webservice/rest.php
@@ -8,9 +8,19 @@ header("Content-Type:application/json");
 if (isset($_GET)) {
 
     if (!empty($_GET["view"])) {
-        if ($_GET["view"] == 'svi_gradovi'){ 
+        if ($_GET["view"] == 'svi_gradovi') {
             sviGradovi();
         }
-        
+    }
+    if (!empty($_GET["metoda"])) {
+        if ($_GET["metoda"] == 'registracijaVolontera') {
+           echo regVolontera($_GET["email"],$_GET["lozinka"],$_GET["oib"],$_GET["grad"],$_GET["adresa"],$_GET["kontakt"],$_GET["ime"],$_GET["prezime"]);
+        }
     }
 }
+
+
+ 
+
+
+//RewriteRule ^registracija/fizicka/([a-zA-Z]+)/([a-zA-Z]+)/([^]]+) rest.php?ime=$1&prezime=$2&email=$3 [nc,qsa]
