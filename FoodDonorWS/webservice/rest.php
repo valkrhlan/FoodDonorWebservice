3e18971b2777d2182880603aca44eb002aa1af3b
valkrhlan@foi.hr <44kF7EXL>
2016-12-06 16:33:35 +0100

    Dodan novi rewrite rule za dodavanje novog paketa

--- FoodDonorWS/webservice/rest.php
+++ FoodDonorWS/webservice/rest.php
@@ -6,6 +6,7 @@ header("Content-Type:application/json");
 
 //echo "rest.php";
 if (isset($_GET)) {
+   
     if (!empty($_GET["view"])) {
         if ($_GET["view"] == 'svi_gradovi') {
             sviGradovi();
@@ -15,6 +16,7 @@ if (isset($_GET)) {
         }
     }
     if (!empty($_GET["metoda"])) {
+         var_dump($_GET);
         if ($_GET["metoda"] == 'prijava') {
             prijava($_GET["email"], $_GET["lozinka"]);
         }
@@ -23,6 +25,13 @@ if (isset($_GET)) {
         }
         if ($_GET["metoda"] == 'registracijaVolontera') {
             regVolontera($_GET["email"], $_GET["lozinka"], $_GET["oib"], $_GET["grad"], $_GET["adresa"], $_GET["kontakt"], $_GET["ime"], $_GET["prezime"]);
+        }
+        if($_GET["metoda"] == 'novi') {
+            echo "tu sam";
+            //$json=$_GET["json"];
+            print_r($_GET);
+           // echo($json);
+           // print_r(json_decode($json));
         }
     }
 }
