85c781ae6573188075b38f6f1fa558771dd2ed7d
valkrhlan <valkrhlan@foi.hr>
2017-01-30 14:20:09 +0100

    Dorađeno dohvaćanje notifikacija za konfigurabilno spajanje.

--- FoodDonorWS/webservice/konfigurabilno_notifikacije.php
+++ FoodDonorWS/webservice/konfigurabilno_notifikacije.php
@@ -4,8 +4,10 @@ include_once("../klase/baza.php");
 include_once("funkcije.php");
 
 function dohvati_obavijesti($email, $ts) {
-
-    $d = date('Y-m-d H:i:s', $ts);
+ 
+    $pomDate=$ts-(60*60);
+    $d = date('Y-m-d H:i:s', $pomDate);
+    
     $sql = "SELECT * FROM korisnik k LEFT JOIN detalji_pravna dp ON k.id=dp.id_korisnik WHERE email='$email'";
     $rez = vrati_podatke($sql);
     $naziv = "";
@@ -15,13 +17,14 @@ function dohvati_obavijesti($email, $ts) {
             $tip = $row["tip"];
         }
 
-        //1 donor
-        //2 volonter
-        //3 potrebiti
+        //1 donor -> kad kreira spremi se v_kreiranja
+        //2 volonter -> kad kreira spremi se v_preuzeto
+        //3 potrebiti -> kad odabere spremi se v_naručeno
+        
         $notifikacije = array();
         $brojac = 0;
         if ($tip == 3) {
-            $sql2 = "SELECT * FROM paketi p JOIN status s ON p.status=s.id_status  LEFT JOIN detalji_pravna dp ON p.id_donor=dp.id_korisnik WHERE p.id_potrebitog IS NULL AND v_kreiranja>'$d'";
+            $sql2 = "SELECT * FROM paketi p JOIN status s ON p.status=s.id_status  LEFT JOIN detalji_pravna dp ON p.id_donor=dp.id_korisnik WHERE v_kreiranja>'$d'";
             $rez2 = vrati_podatke($sql2);
             if ($rez2->num_rows > 0) {
                 while ($row2 = $rez2->fetch_assoc()) {
@@ -31,6 +34,36 @@ function dohvati_obavijesti($email, $ts) {
                 }
             }
         }
+        elseif ($tip==2) {
+            $sql2 = "SELECT * FROM paketi p JOIN status s ON p.status=s.id_status  LEFT JOIN detalji_pravna dp ON p.id_potrebitog=dp.id_korisnik WHERE v_naruceno>'$d'";
+            $rez2 = vrati_podatke($sql2);
+            if ($rez2->num_rows > 0) {
+                while ($row2 = $rez2->fetch_assoc()) {
+                    //var_dump($row["hitno"]);
+                    if($row2["hitno"]!=NULL){
+                       $pom_notif = array('title' => 'HITNO! Prijevoz ', 'message' => $row2["naziv"] . " treba prijevoz paketa!");
+                    }else{
+                         $pom_notif = array('title' => 'Prijevoz ', 'message' => $row2["naziv"] . " treba prijevoz paketa!");
+                    }
+                    array_push($notifikacije, $pom_notif);
+                    $brojac++;
+                }
+            }    
+        }
+        elseif ($tip==1) {
+            $sql2 = "SELECT * FROM paketi p JOIN status s ON p.status=s.id_status LEFT JOIN korisnik k ON p.id_volonter=k.id WHERE id_donor !=id_volonter AND v_preuzeto>'$d'";
+            $rez2 = vrati_podatke($sql2);
+            if ($rez2->num_rows > 0) {
+                while ($row2 = $rez2->fetch_assoc()) {
+                    $pom_notif = array('title' => 'Prijevoz', 'message' => $row2["ime"]." ".$row2["prezime"] . " će prevesti Vaš paket!");
+                    array_push($notifikacije, $pom_notif);
+                    $brojac++;
+                }
+            }  
+        }
+        
+    
+        
         if ($brojac == 0) {
             deliver_response("OK", $brojac, "Nema ni jedne nove obavijesti!", array('notifikacije' => 'nema'));
         } else {
